624e53128a654da42c801a814a243d38c2afae6f
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-05-06 15:55:00 +0300

    matching condition

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const time = document.querySelector("#timer")
 const scores = document.querySelector("#score")
+const cards = document.querySelectorAll(".cards")
 
 const player1 = localStorage.getItem("player1")
 console.log(`player 1 => ${player1}`)
@@ -7,12 +8,17 @@ console.log(`player 1 => ${player1}`)
 const player2 = localStorage.getItem("player2")
 console.log(`player 2 => ${player2}`)
 
-// start & stop timer
-
 let seconds = 0
 let timer
 let isGameActive = false
 let endTimerMessage = "Time is Up."
+
+let score = 0
+let currentTurn = 1
+
+let flippedCard = false
+let firstCard, secondCard // to choose first then seconed card to see matching cards
+let lockBoard = false // set to false so when the player click the seconed card it turnes to true
 
 const displayMessage = (message) => {
   // Show message here. You can use an alert box for example.
@@ -23,7 +29,6 @@ const startTimer = () => {
   isGameActive = true
   timer = setInterval(() => {
     seconds++
-    console.log(seconds)
     time.innerText = `${seconds} sec`
     checkTimer()
   }, 1000)
@@ -51,27 +56,76 @@ const playAudio = () => {
   myAudio.play()
 }
 
-///fil cards
-const flipCards = document.querySelectorAll(".cards")
-
-flipCards.forEach((cards) => {
-  cards.addEventListener("click", () => {
-    isGameActive && cards.classList.toggle("flip")
+const flipCardsListeners = () => {
+  cards.forEach((card) => {
+    card.addEventListener("click", flipCard)
   })
-})
+}
 
-/// change name
+const matchLogic = (card) => {
+  // Is this the first card?
+  if (!firstCard) {
+    firstCard = card
+  } else {
+    secondCard = card
+    checkForMatch()
+  }
+}
 
 // const showNames = () => {
+//   // TODO Fix this
 //   document.getElementById("playerName1").innerHTML = "newText"
 //   document.getElementById("playerName2").innerHTML = "newText"
 // }
 
+const flipCard = (event) => {
+  console.log(event.target)
+  if (isGameActive) {
+    event.target.parentElement.classList.toggle("flip")
+    matchLogic(event.target)
+  }
+}
+
+// const flipCard = (card) => {
+//   if (isGameActive) {
+//     card.classList.toggle("flip")
+//     matchLogic(card)
+//   }
+// }
+
+const checkForMatch = () => {
+  console.log(`framework 1 => ${firstCard.dataset.framework}`)
+  console.log(`framework 2 => ${secondCard.dataset.framework}`)
+  if (
+    firstCard.parentElement.dataset.framework ===
+    secondCard.parentElement.dataset.framework
+  ) {
+    console.log("match!")
+    disableCards() // if there is matching
+    score += 5
+  } else {
+    unflipCards() // turns both back if there is no matching
+  }
+}
+
+const disableCards = () => {
+  // add event listener to check for match
+  firstCard.parentElement.removeEventListener("click", flipCard)
+  secondCard.parentElement.removeEventListener("click", flipCard)
+}
+
+const unflipCards = () => {
+  // turns both back if there is no matching cards
+  setTimeout(() => {
+    firstCard.parentElement.classList.remove("flip")
+    secondCard.parentElement.classList.remove("flip")
+  }, 1000)
+}
+
 const startGame = () => {
   startTimer()
   // showNames()
+  flipCardsListeners()
 }
 
 startGame()
-
-// add winning condition and player turns
